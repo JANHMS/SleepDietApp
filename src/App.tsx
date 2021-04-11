d66bf7a32edba76edd6f32d01aa587294f4ddc52
JANHMS <jan@kooperativ.net>
2021-04-11 12:52:50 +0200

    feat: Drag Drop file

--- src/App.tsx
+++ src/App.tsx
@@ -33,6 +33,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import UploadCSVPage from './pages/UploadCSVPage';
 
 const App: React.FC = () => (
   <IonApp>
@@ -41,6 +42,9 @@ const App: React.FC = () => (
         <IonRouterOutlet>
           <Route exact path="/home">
             <HomeTab />
+          </Route>
+          <Route exact path="/food/upload">
+            <UploadCSVPage />
           </Route>
           <Route exact path="/food">
             <AddFoodTab />
