99e342ef825c8a363c04bcbe7e337297ab2f3c36
Muraközy Nóra <murakozy.nora@gmail.com>
2021-04-13 21:14:36 +0200

    Categories are toggable, install clsx
    - clsx is for listing multiple css classNames on components, even
      conditional ones
    - categories can be toggled, a white border appears on click, and
      disappears after another click

--- src/components/add-food-components/Category.tsx
+++ src/components/add-food-components/Category.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     IonCard,
     IonCardTitle,
 } from '@ionic/react';
 import "./Category.css"
+import clsx from "clsx";
 
 interface CategoryProps {
     name: string;
@@ -11,9 +12,12 @@ interface CategoryProps {
 }
 
 export const Category: React.FC<CategoryProps> = ({name, cssClassName}) => {
+    const [isSelected, setSelected] = useState<boolean>(false);
+
     return (
         <div>
-            <IonCard button type="button" className={cssClassName}/>
+            <IonCard button type="button" className={clsx(isSelected && "selected", cssClassName)}
+                     onClick={e => setSelected(!isSelected)}/>
             <IonCardTitle>{name}</IonCardTitle>
         </div>
     );
