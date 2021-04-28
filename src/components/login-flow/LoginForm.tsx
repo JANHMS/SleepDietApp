22d1b331aae6a283f82623d47ecf5b297fc4def9
Muraközy Nóra <murakozy.nora@gmail.com>
2021-04-28 18:44:13 +0200

    Fix login form and toast to red when login fails

--- src/components/login-flow/LoginForm.tsx
+++ src/components/login-flow/LoginForm.tsx
@@ -1,63 +1,91 @@
-import { IonContent, IonLoading, IonInput, IonButton, IonHeader, IonPage, IonToolbar, IonList, IonItem, IonLabel, IonText } from '@ionic/react';
+import {
+    IonContent,
+    IonLoading,
+    IonInput,
+    IonButton,
+    IonPage,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonText,
+    IonTitle,
+} from '@ionic/react';
 import React from 'react';
-import HeaderImage from './HeaderImage';
+import "./LoginForm.css"
 
 interface Props {
-  email: string;
-  setEmail: (x: string) => void;
-  password: string;
-  setPassword: (x: string) => void;
-  status: any;
-  handleLogin: any;
-  auth: any;
+    email: string;
+    setEmail: (x: string) => void;
+    password: string;
+    setPassword: (x: string) => void;
+    status: any;
+    handleLogin: any;
+    auth: any;
 }
 
 const LoginForm: React.FC<Props> = ({
-  email,
-  setEmail,
-  password,
-  setPassword,
-  status,
-  handleLogin,
-  auth
-}) => {
-  return(
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-        <HeaderImage />
-        </IonToolbar>
-      </IonHeader>
-      <h2>Login</h2>
-      <IonContent className="ion-padding">
-        <IonList>
-          <IonItem>
-            <IonLabel position="stacked">Email</IonLabel>
-            {/*Fix error: Argument of type 'string | null | undefined' is not assignable to parameter of type 'string'. Type 'undefined' is not assignable to type 'string'*/}
-            {/*Add "!" and convince TS that the value won't be null or undefined*/}
-            <IonInput type="email" value={email}
-              onIonChange={(event) => setEmail(event.detail.value!)}
-            />
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">Password</IonLabel>
-            {/*Same as above*/}
-            <IonInput type="password" value={password}
-              onIonChange={(event) => setPassword(event.detail.value!)}
-            />
-          </IonItem>
-        </IonList>
-        {status.error &&
-          <IonText color="danger">Invalid credentials</IonText>
-        }
-        <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
-        <IonButton expand="block" fill="clear" routerLink="/register">
-          Don't have an account?
-        </IonButton>
-        <IonLoading isOpen={status.loading} />
-      </IonContent>
-    </IonPage>
-  )
+                                        email,
+                                        setEmail,
+                                        password,
+                                        setPassword,
+                                        status,
+                                        handleLogin,
+                                        auth
+                                    }) => {
+    return (
+        <IonPage className="introPage">
+            <IonContent className="pageContent">
+                <IonTitle className="title">
+                    Sleepathy
+                </IonTitle>
+                <IonText className="subtitle">
+                    <h1>Login</h1>
+                </IonText>
+                <IonList className="inputList">
+                    <IonItem>
+                        <IonLabel position="floating">E-mail</IonLabel>
+                        {/*Fixed error: Argument of type 'string | null | undefined' is not assignable to parameter of type 'string'. Type 'undefined' is not assignable to type 'string'*/}
+                        {/*Add "!" and convince TS that the value won't be null or undefined*/}
+                        <IonInput type="email"
+                                  value={email}
+                                  // color={status.error && "danger"}
+                                  onIonChange={(event) => setEmail(event.detail.value!)}
+                        />
+                    </IonItem>
+                    <IonItem>
+                        <IonLabel position="floating">Password</IonLabel>
+                        {/*Same as above*/}
+                        <IonInput type="password"
+                                  value={password}
+                                  // color={status.error && "danger"}
+                                  onIonChange={(event) => setPassword(event.detail.value!)}
+                        />
+                    </IonItem>
+                </IonList>
+                <div className="buttonContainer">
+                    <IonButton
+                        fill="solid"
+                        // size="small"
+                        className="btnLogin"
+                        onClick={handleLogin}>
+                        Login
+                    </IonButton>
+                    <div className="signUpContainer">
+                        <IonText className="doNotHaveAccountText">
+                            <p>Don't have an account?</p>
+                        </IonText>
+                        <IonButton
+                            fill="clear"
+                            size="small"
+                            routerLink="/register">
+                            Register
+                        </IonButton>
+                    </div>
+                </div>
+                <IonLoading isOpen={status.loading}/>
+            </IonContent>
+        </IonPage>
+    )
 }
 
 export default LoginForm;
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,46 +1,52 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router';
-import { useAuth } from '../auth';
+import React, {useState} from 'react';
+import {Redirect} from 'react-router';
+import {useAuth} from '../auth';
 import LoginForm from '../components/login-flow/LoginForm';
-import { auth } from '../firebase';
-import { toast } from '../toast';
-import { useHistory } from "react-router-dom";
+import {auth} from '../firebase';
+import {useHistory} from "react-router-dom";
+import {useIonToast} from "@ionic/react";
 
 const LoginPage: React.FC = () => {
-  const { loggedIn } = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [status, setStatus] = useState({ loading: false, error: false });
-  const history = useHistory();
-  const handleLogin = async () => {
-    try {
-      setStatus({ loading: true, error: false });
-      const credential = await auth.signInWithEmailAndPassword(email, password);
-      console.log('credential:', credential);
-      toast("Logged in")
-      history.push("/my/home")
-      setStatus({ loading: false, error: false });
-    } catch (error) {
-      setStatus({ loading: false, error: true });
-      console.log('error:', error);
-      toast("Login failed")
-    }
-  };
+    const {loggedIn} = useAuth();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [status, setStatus] = useState({loading: false, error: false});
+    const [present, dismiss] = useIonToast();
+    const history = useHistory();
+    const handleLogin = async () => {
+        try {
+            setStatus({loading: true, error: false});
+            const credential = await auth.signInWithEmailAndPassword(email, password);
+            console.log('credential:', credential);
+            // toast("Logged in")
+            history.push("/my/home")
+            setStatus({loading: false, error: false});
+        } catch (error) {
+            setStatus({loading: false, error: true});
+            console.log('error:', error);
+            present({
+                buttons: [{text: 'OK', handler: () => dismiss()}],
+                message: 'Invalid credentials',
+                duration: 2500,
+                color: "danger"
+            })
+        }
+    };
 
-  if (loggedIn) {
-    return <Redirect to="/my/home" />;
-  }
-  return (
-    <LoginForm
-      email={email}
-      setEmail={setEmail}
-      password={password}
-      setPassword={setPassword}
-      status={status}
-      handleLogin={handleLogin}
-      auth={auth}
-    />
-  );
+    if (loggedIn) {
+        return <Redirect to="/my/home"/>;
+    }
+    return (
+        <LoginForm
+            email={email}
+            setEmail={setEmail}
+            password={password}
+            setPassword={setPassword}
+            status={status}
+            handleLogin={handleLogin}
+            auth={auth}
+        />
+    );
 };
 
 export default LoginPage;
