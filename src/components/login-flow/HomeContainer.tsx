627a54d028c3430217a713cc8a7b4e7682332170
Muraközy Nóra <murakozy.nora@gmail.com>
2021-04-29 21:51:41 +0200

    Transfer home logic to HomeContainer, HomeTab only contains the skeleton

--- src/components/login-flow/HomeContainer.tsx
+++ src/components/login-flow/HomeContainer.tsx
@@ -1,111 +1,170 @@
-import {IonButton, IonTitle, IonText, IonIcon} from '@ionic/react';
-import React from 'react';
+import {IonButton, IonTitle, IonText, IonIcon, IonLoading} from '@ionic/react';
+import React, {useEffect, useState} from 'react';
 import './HomeContainer.css';
 import DonutChartContainer from '../../charts/DonutChartContainer';
 import cat from '../../images/cat.png';
 import moment from "moment/moment";
 import {logOutOutline} from "ionicons/icons";
+import {useHistory} from "react-router";
+import {auth, firestore} from "../../firebase";
 
-interface Props {
-    logout: () => void,
-    sleep: any;
-    food: any;
-    DinnerTime: string;
-}
+const HomeContainer: React.FC = () => {
+    const history = useHistory()
+    const userId = "1";
 
-const HomeContainer: React.FC<Props> = ({
-                                            logout,
-                                            food,
-                                            sleep,
-                                            DinnerTime
-                                        }) => {
-    return (
-        <div className="homeContainer">
-            <div className="headerContainer">
-                <IonIcon icon={logOutOutline} onClick={logout} className="logoutIcon"/>
-                <IonTitle>{moment().format('dddd, MMMM Do')}</IonTitle>
-            </div>
-            <div className="sleepTimeContainer">
-                <IonText className="text">
-                    {moment(sleep.Start.split(' ')[1], 'HH:mm:ss').format('HH:mm')}
-                    <br/>
-                    <b>Sleep started</b>
-                </IonText>
-                <IonText className="text">
-                    {moment(sleep.End.split(' ')[1], 'HH:mm:ss').format('HH:mm')}
-                    <br/>
-                    <b>Woke up at</b>
-                </IonText>
-                <IonText className="text">
-                    {moment(DinnerTime, 'HH:mm:ss').format('HH:mm')}
-                    <br/>
-                    <b>Dinner time</b>
-                </IonText>
-            </div>
-            <div className="foodDetailsContainer">
-                <div className="right">
-                    {food.Category.map((category: string, index: number) => (
-                        <IonText key={category} className="categoryListElement text">
-                            {category}{index !== food.Category.length - 1 ? ", " : ""}
+    const [loading, setLoading] = useState(true)
+    const [sleep, setSleep] = useState<any>();
+    const [food, setFood] = useState<any>();
+    const [DinnerTime, setDinnerTime] = useState<any>();
+
+    const [user, setUser] = useState<any>();
+
+    useEffect(() => {
+        firestore.collection("profiles").doc(userId)
+            .onSnapshot(async (doc) => {
+                // var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+                const userData = doc.data()
+                await setUser(userData)
+            })
+
+        const sleepPromise = new Promise((resolve, reject) => {
+                resolve(
+                    firestore.collection("users").doc(userId).collection('sleep')
+                        .orderBy("End", "desc")
+                        .get()
+                        .then(doc => {
+                            const sleepData = doc.docs.map(doc => ({id: doc.id, ...doc.data()}))
+                            setSleep(sleepData[1])
+                            console.log(userId)
+                            firestore.collection("users").doc(userId).collection('food')
+                                .orderBy("Date", "desc")
+                                .get()
+                                .then(doc => {
+                                    const foodData = doc.docs.map(doc => ({id: doc.id, ...doc.data()}))
+                                    setFood(foodData[1])
+                                    // console.log("This is sleep", sleep)
+                                    // console.log("This is food", food)
+                                })
+                        })
+                )
+            }
+        )
+
+        sleepPromise.then(() => {
+            setLoading(false)
+        })
+    }, [])
+
+    useEffect(() => {
+        console.log("This is sleep", sleep)
+    }, [sleep])
+
+    useEffect(() => {
+        console.log("This is food", food)
+        if (food !== undefined) {
+            const dinnerTime = food.Date.split(' ')[1]
+            setDinnerTime(dinnerTime)
+        } else return;
+    }, [food])
+
+    async function logout() {
+        await auth.signOut()
+        // toast("Logged out")
+        history.push('/')
+    }
+
+    return (sleep && food && DinnerTime ? (
+                <div className="homeContainer">
+                    <div className="headerContainer">
+                        <IonIcon icon={logOutOutline} onClick={logout} className="logoutIcon"/>
+                        <IonTitle>{moment().format('dddd, MMMM Do')}</IonTitle>
+                    </div>
+                    <div className="sleepTimeContainer">
+                        <IonText className="text">
+                            {moment(sleep.Start.split(' ')[1], 'HH:mm:ss').format('HH:mm')}
+                            <br/>
+                            <b>Sleep started</b>
                         </IonText>
-                    ))}
-                    <br/>
-                    <IonText className="text"><b>Categories</b></IonText>
-                </div>
-            </div>
+                        <IonText className="text">
+                            {moment(sleep.End.split(' ')[1], 'HH:mm:ss').format('HH:mm')}
+                            <br/>
+                            <b>Woke up at</b>
+                        </IonText>
+                        <IonText className="text">
+                            {moment(DinnerTime, 'HH:mm:ss').format('HH:mm')}
+                            <br/>
+                            <b>Dinner time</b>
+                        </IonText>
+                    </div>
+                    <div className="foodDetailsContainer">
+                        <div className="right">
+                            {food.Category.map((category: string, index: number) => (
+                                <IonText key={category} className="categoryListElement text">
+                                    {category}{index !== food.Category.length - 1 ? ", " : ""}
+                                </IonText>
+                            ))}
+                            <br/>
+                            <IonText className="text"><b>Categories</b></IonText>
+                        </div>
+                    </div>
 
-            <div>
-                <DonutChartContainer score={parseInt(sleep.Sleep_quality)}/>
-            </div>
+                    <div>
+                        <DonutChartContainer score={parseInt(sleep.Sleep_quality)}/>
+                    </div>
 
-            <div className="asleepMovementContainer">
-                <IonText className="left text">
-                    <IonText>
-                        {moment(sleep.Time_asleep, 'HH:mm:ss').format('HH:mm')}
-                    </IonText>
-                    <br/>
-                    <b>Sleep duration</b>
-                </IonText>
-                <IonText className="right text">
-                    <IonText>
-                        {sleep.Movements_per_hour}
-                    </IonText>
-                    <br/>
-                    <b>Movements / hour</b>
-                </IonText>
-            </div>
+                    <div className="asleepMovementContainer">
+                        <IonText className="left text">
+                            <IonText>
+                                {moment(sleep.Time_asleep, 'HH:mm:ss').format('HH:mm')}
+                            </IonText>
+                            <br/>
+                            <b>Sleep duration</b>
+                        </IonText>
+                        <IonText className="right text">
+                            <IonText>
+                                {sleep.Movements_per_hour}
+                            </IonText>
+                            <br/>
+                            <b>Movements / hour</b>
+                        </IonText>
+                    </div>
 
-            <div className="regTimeInBedContainer">
-                <IonText>
-                    <IonText className="left text">
-                        {sleep.Regularity}
-                    </IonText>
-                    <br/>
-                    <IonText className="text"><b>Regularity</b></IonText>
-                </IonText>
-                <IonText className="right text">
-                    <IonText>
-                        {moment(sleep.Time_in_bed, 'HH:mm:ss').format('HH:mm')}
-                    </IonText>
-                    <br/>
-                    <b>Time in bed</b>
-                </IonText>
-            </div>
-            <IonButton href="/food" size="small" className="btnAddFood">Add Food</IonButton>
+                    <div className="regTimeInBedContainer">
+                        <IonText>
+                            <IonText className="left text">
+                                {sleep.Regularity}
+                            </IonText>
+                            <br/>
+                            <IonText className="text"><b>Regularity</b></IonText>
+                        </IonText>
+                        <IonText className="right text">
+                            <IonText>
+                                {moment(sleep.Time_in_bed, 'HH:mm:ss').format('HH:mm')}
+                            </IonText>
+                            <br/>
+                            <b>Time in bed</b>
+                        </IonText>
+                    </div>
+                    <IonButton href="/food" size="small" className="btnAddFood">Add Food</IonButton>
 
-            {/*<div style={{*/}
-            {/*    position: "absolute",*/}
-            {/*    marginTop: "5%",*/}
-            {/*    marginLeft: "39%",*/}
-            {/*    textAlign: "center",*/}
-            {/*    textOverflow: "ellipsis",*/}
-            {/*    whiteSpace: "nowrap",*/}
-            {/*    overflow: "hidden",*/}
-            {/*    width: "80px"*/}
-            {/*}}>*/}
-            {/*    <img src={cat}/>*/}
-            {/*</div>*/}
-        </div>
+                    {/*<div style={{*/}
+                    {/*    position: "absolute",*/}
+                    {/*    marginTop: "5%",*/}
+                    {/*    marginLeft: "39%",*/}
+                    {/*    textAlign: "center",*/}
+                    {/*    textOverflow: "ellipsis",*/}
+                    {/*    whiteSpace: "nowrap",*/}
+                    {/*    overflow: "hidden",*/}
+                    {/*    width: "80px"*/}
+                    {/*}}>*/}
+                    {/*    <img src={cat}/>*/}
+                    {/*</div>*/}
+                </div>)
+            :
+            (
+                <IonLoading isOpen={loading}/>
+            )
+
     );
 };
 
--- src/pages/HomeTab.tsx
+++ src/pages/HomeTab.tsx
@@ -1,94 +1,16 @@
-import {IonButton, IonContent, IonHeader, IonLoading, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import {IonContent, IonPage} from '@ionic/react';
 import './HomeTab.css';
 import HomeContainer from "../components/login-flow/HomeContainer";
-import React, {useState, useEffect} from 'react';
-import {firestore} from '../firebase';
-import {auth} from '../firebase';
-import {useHistory} from "react-router";
-import {toast} from '../toast';
-import {useAuthInit} from "../auth";
+import React from 'react';
 
 const HomeTab: React.FC = () => {
-    const history = useHistory()
-    const userId = "1";
-
-    const [loadingLogout, setLoadingLogout] = useState(false)
-    const [loading, setLoading] = useState(true)
-    const [sleep, setSleep] = useState<any>();
-    const [food, setFood] = useState<any>();
-    const [DinnerTime, setDinnerTime] = useState<any>();
-
-    const [user, setUser] = useState<any>();
-
-    useEffect(() => {
-        firestore.collection("profiles").doc(userId)
-            .onSnapshot(async (doc) => {
-                // var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-                const userData = doc.data()
-                await setUser(userData)
-            })
-
-        const sleepPromise = new Promise((resolve, reject) => {
-                resolve(
-                    firestore.collection("users").doc(userId).collection('sleep')
-                        .orderBy("End", "desc")
-                        .get()
-                        .then(doc => {
-                            const sleepData = doc.docs.map(doc => ({id: doc.id, ...doc.data()}))
-                            setSleep(sleepData[1])
-                            console.log(userId)
-                            firestore.collection("users").doc(userId).collection('food')
-                                .orderBy("Date", "desc")
-                                .get()
-                                .then(doc => {
-                                    const foodData = doc.docs.map(doc => ({id: doc.id, ...doc.data()}))
-                                    setFood(foodData[1])
-                                    // console.log("This is sleep", sleep)
-                                    // console.log("This is food", food)
-                                })
-                        })
-                )
-            }
-        )
-
-        sleepPromise.then(() => {
-            setLoading(false)
-        })
-    }, [])
-
-    useEffect(() => {
-        console.log("This is sleep", sleep)
-    }, [sleep])
-
-    useEffect(() => {
-        console.log("This is food", food)
-        if (food !== undefined) {
-            const dinnerTime = food.Date.split(' ')[1]
-            setDinnerTime(dinnerTime)
-        } else return;
-    }, [food])
-
-    async function logout() {
-        setLoadingLogout(true)
-        await auth.signOut()
-        // toast("Logged out")
-        setLoadingLogout(false)
-        history.push('/')
-    }
-
-    return (sleep && !loading && food && DinnerTime ?
-            (
-                <IonPage>
-                    <IonContent overflow-scroll="false" className="homeTabContent">
-                        <HomeContainer logout={logout} sleep={sleep} food={food} DinnerTime={DinnerTime}/>
-                    </IonContent>
-                </IonPage>
-            )
-            :
-            (
-                <IonLoading isOpen={loading}/>
-            )
-    );
+    return (
+        <IonPage>
+            <IonContent overflow-scroll="false" className="homeTabContent">
+                <HomeContainer/>
+            </IonContent>
+        </IonPage>
+    )
 };
 
 export default HomeTab;
